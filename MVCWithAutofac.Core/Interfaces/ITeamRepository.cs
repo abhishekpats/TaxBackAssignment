ffcbcaedd8fe1c77e91d20cddb064bab4b5037f3
Abhishek Patil <abhishekpatil33@yahoo.com>
2017-09-03 17:18:44 +0530

    Pushing all the required dll files.

--- MVCWithAutofac.Core/Interfaces/ITeamRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using MVCWithAutofac.Core.Model;
-using System;
-namespace MVCWithAutofac.Core.Interfaces
-{
-    public interface ITeamRepository : IRepository
-    {
-        System.Collections.Generic.List<User> GetUsersInTeam(int teamId);
-    }
-}
--- MVCWithAutofac.Core/Interfaces/IUnitOfWork.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace MVCWithAutofac.Core.Interfaces
-{
-    public interface IUnitOfWork : IDisposable
-    {
-        void CommitTransaction();
-        void StartTransaction();
-    }
-}
